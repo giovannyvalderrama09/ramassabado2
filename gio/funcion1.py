f2e0092dc9e99fff1807d313d2159a3ed7af182f
209 <209@academicos.cesde.edu.co>
2022-09-24 11:38:21 -0500

    se crea una funcion

--- gio/funcion1.py
+++ gio/funcion1.py
@@ -1,1 +1,15 @@
 #Funciones tradicionales con python
+
+# Declaracion de una funcion
+
+def saludar(nombre):
+    print(f"Hola {nombre}")
+    
+
+#Llamar o invocar la funcion
+nombre = "Gio"
+saludar(nombre)
+
+   
+    
+
